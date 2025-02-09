c8db7c951eb9d7cf60731a017764efbb3e24f08c
Erik <65229112+EjayGit@users.noreply.github.com>
2025-02-09 13:20:06 +0000

    Update App.jsx

--- firstApp/01-starting-project/src/App.jsx
+++ firstApp/01-starting-project/src/App.jsx
@@ -1,37 +1,8 @@
-import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png'
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-    );
-}
-
-function CoreConcept({image, title, description}){
-  return (
-  <li>
-    <img src={image} alt={title}/>
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
-  );
-}
+import Header from './components/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx'
+import TabButton from '../components/TabButton';
 
 function App() {
   return (
@@ -54,6 +25,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+          </menu>
         </section>
         <h2>Time to get started!</h2>
       </main>
