c12c890c883ca6dfda3798af8f8429e1338f40d6
Erik <65229112+EjayGit@users.noreply.github.com>
2025-02-15 20:49:26 +0000

    Update App.jsx

--- firstApp/01-starting-project/src/App.jsx
+++ firstApp/01-starting-project/src/App.jsx
@@ -1,13 +1,19 @@
 import componentsImg from './assets/components.png'
 import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header.jsx'
-import CoreConcept from './components/CoreConcept.jsx'
+import Header from '../components/Header.jsx'
+import CoreConcept from '../components/CoreConcept.jsx'
 import TabButton from '../components/TabButton';
 
 function App() {
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton)
+  }
+
+
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
@@ -22,15 +28,19 @@ function App() {
               description={CORE_CONCEPTS[1].description}
               image={CORE_CONCEPTS[1].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={function() {handleSelect('jsx')}}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
         <h2>Time to get started!</h2>
       </main>
