1544f8f3a782cd3fd3dfb960b48202554a8c52e5
Erik <65229112+EjayGit@users.noreply.github.com>
2025-02-24 07:13:47 +0000

    Update App.jsx

--- firstApp/01-starting-project/src/App.jsx
+++ firstApp/01-starting-project/src/App.jsx
@@ -4,6 +4,7 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from '../components/Header.jsx'
 import CoreConcept from '../components/CoreConcept.jsx'
 import TabButton from '../components/TabButton';
+import { EXAMPLES } from './data.js';
 
 function App() {
 
@@ -46,7 +47,15 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
         <h2>Time to get started!</h2>
       </main>
