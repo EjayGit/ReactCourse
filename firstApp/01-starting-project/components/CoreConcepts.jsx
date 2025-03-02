2a31877fa7d6692888b722025cc44cf029574f80
Erik <65229112+EjayGit@users.noreply.github.com>
2025-03-02 15:51:51 +0000

    Add files via upload

---
+++ firstApp/01-starting-project/components/CoreConcepts.jsx
@@ -0,0 +1,24 @@
+import CoreConcept from './CoreConcept.jsx';
+
+export default function CoreConcepts(){
+    return (
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map(conceptItem => <CoreConcept key={conceptItem.title}{...conceptItem}/>)}
+{/*             <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+          </ul>
+        </section>
+    )
+}
